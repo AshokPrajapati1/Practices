d207af66c62b86b082e33518f68b88c72736301e
unknown <Ashok@gmail.com>
2024-06-24 11:12:29 +0530

    2nd

--- Main.tf
+++ Main.tf
@@ -2,3 +2,9 @@ resource "azurerm_resource_group" "example" {
   name     = "Prajapati-resources-01g"
   location = "West  US "
 }
+
+resource "azurerm_resource_group" "example" {
+  name     = "Prajapati-resources-01g"
+  location = "West  US "
+}
+
